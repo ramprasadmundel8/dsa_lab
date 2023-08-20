af24216de729abd969391a156a0f5a4399ac93f6
ramprasad mundel <118931691+ramprasadmundel8@users.noreply.github.com>
2023-08-21 01:20:58 +0530

    p6(B).c

---
+++ p6(B).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void towerOfHanoi(int n,char from_rod,char to_rod,char aux_rod){
+if(n==1){
+printf("\nMove disk %d from rod %c to rod %c",n,
+from_rod,to_rod); 
+return; 
+}
+towerOfHanoi(n-1,from_rod,
+aux_rod,to_rod); 
+printf("\nMove disk %d from rod %c to rod %c",n,
+from_rod,to_rod); 
+towerOfHanoi(n-1,
+aux_rod,to_rod,
+from_rod); 
+}
+int main() {
+int n=4; //Number of disks
+towerOfHanoi(n,'A','C','B'); //A,B,C are names of rods
+return 0; 
+}
