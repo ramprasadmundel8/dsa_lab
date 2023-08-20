5f6c6470ece46c31f03543cab5128c8be6176fba
ramprasad mundel <118931691+ramprasadmundel8@users.noreply.github.com>
2023-08-21 01:20:05 +0530

    p6(A).c

---
+++ p6(A).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int binarySearch(int arr[], int low, int high, int key) {
+if (high >= low) {
+int mid = low + (high - low) / 2;
+if (arr[mid] == key)
+return mid;
+if (arr[mid] > key)
+return binarySearch(arr, low, mid - 1, key);
+return binarySearch(arr, mid + 1, high, key);
+}
+return -1;
+}
+int main() {
+int arr[] = {2, 3, 4, 10, 40};
+int n = sizeof(arr) / sizeof(arr[0]);
+int key = 10;
+int result = binarySearch(arr, 0, n - 1,key);
+if(result==-1)
+printf("Element is not present in array");
+else
+printf("Element is present at index %d",result);
+return 0; 
+}
